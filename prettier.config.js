4f2c5ef0a5be041eea6946e2b03647f1b451511f
iklimkin <ivan.klimkin@1win.pro>
2025-11-15 15:13:56 +0300

    Merge updates from main branch to synchronize changes

---
+++ prettier.config.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  printWidth: 80,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  arrowParens: 'always',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  bracketSameLine: false,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  insertPragma: false,
+};
