8ea1f47471e2a89ebb798fe1468d6e72c031ca54
iklimkin <ivan.klimkin@1win.pro>
2025-11-16 16:17:45 +0300

    refactor: extract record parsing logic into a reusable function for create and update methods

--- db.js
+++ db.js
@@ -10,6 +10,18 @@ const pool = new pg.Pool({
   database: 'example',
 });
 
+const parseRecord = (record, parser) => {
+  const keys = Object.keys(record);
+  const nums = Array(keys.length);
+  const data = Array(keys.length);
+  let i = 0;
+  for (const key of keys) {
+    data[i] = record[key];
+    nums[i] = parser(++i, key);
+  }
+  return { keys, data, nums };
+};
+
 const db = (table) => ({
   async query(sql, args) {
     return pool.query(sql, args);
@@ -29,34 +41,18 @@ const db = (table) => ({
   },
 
   async create({ ...record }) {
-    const keys = Object.keys(record);
-    const nums = Array(keys.length);
-    const data = Array(keys.length);
-    let i = 0;
-    for (const key of keys) {
-      data[i] = record[key];
-      nums[i] = `$${++i}`;
-    }
-    console.log('keys', keys);
+    const { keys, nums, data } = parseRecord(record, (i) => `$${i}`);
     const fields = `"${keys.join('", "')}"`;
-    console.log('fields', fields);
     const params = nums.join(', ');
     const sql = `INSERT INTO ${table} (${fields}) VALUES (${params}) RETURNING id`;
-    const result = await this.query(`${sql}`, data);
+    const result = await this.query(sql, data);
     return result.rows[0].id;
   },
 
   async update(id, { ...record }) {
-    const keys = Object.keys(record);
-    const updates = Array(keys.length);
-    const data = Array(keys.length);
-    let i = 0;
-    for (const key of keys) {
-      data[i] = record[key];
-      updates[i] = `${key} = $${++i}`;
-    }
-    const delta = updates.join(', ');
-    const sql = `UPDATE ${table} SET ${delta} WHERE id = $${++i} RETURNING id, login`;
+    const { nums, data } = parseRecord(record, (i, key) => `${key} = $${i}`);
+    const delta = nums.join(', ');
+    const sql = `UPDATE ${table} SET ${delta} WHERE id = $${nums.length + 1} RETURNING id, login`;
     data.push(id);
     const result = await this.query(sql, data);
     return result.rows[0];
