9e1ff50e3167f7ce38f39197e27cf31902e71ea1
iklimkin <ivan.klimkin@1win.pro>
2025-11-16 13:01:24 +0300

    feat: implement body parser utility and remove unused express dependency

---
+++ body.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const bodyParser = async (req) => {
+  const buffers = [];
+  for await (const chunk of req) buffers.push(chunk);
+  const data = Buffer.concat(buffers).toString();
+  return JSON.parse(data);
+};
+
+module.exports = bodyParser;
